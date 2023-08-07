d7c0d6b0b72769415d7f3357a0590985120626dd
strange-world <eric.lawrence789@gmail.com>
2023-08-07 11:13:23 -0500

    123

--- HelloWorld/HelloWorld.sol
+++ HelloWorld/HelloWorld.sol
@@ -1,12 +1,17 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 contract HelloWorld {
-    uint public value;
+    uint private value;
 
-    function setValue(uint newValue) public{
+    function setValue(uint newValue) public {
         value = newValue;
     }
+
+    function getValue() public view returns (uint) {
+        return value;
+    }
+
     function remove() public {
         selfdestruct(payable(address(0x0)));
     }

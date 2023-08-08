32b9ddc412035720f04b67d2deebeabd683f2636
Eric Lawrence <134732988+strange-world@users.noreply.github.com>
2023-08-08 12:32:53 -0500

    Update contract_interaction.js

--- Voter/contract_interaction.js
+++ Voter/contract_interaction.js
@@ -1,56 +1,60 @@
-let fs = require('fs')
-let Web3 = require('web3')
+// Import required modules
+let fs = require('fs');                  // File system module to read files
+let Web3 = require('web3');              // Web3 library for interacting with Ethereum
 
-require('dotenv').config()
+// Load environment variables
+require('dotenv').config();             // Load environment variables from a .env file
 
-// Read contract's ABI
-const abiStr = fs.readFileSync('Voter_sol_Voter.abi', 'utf8')
-const abi = JSON.parse(abiStr)
+// Read the contract's ABI (Application Binary Interface)
+const abiStr = fs.readFileSync('Voter_sol_Voter.abi', 'utf8');   // Read the ABI from a file
+const abi = JSON.parse(abiStr);          // Parse the ABI into a JSON object for later use
 
 // Create Web3 instance
-const web3 = new Web3()
-web3.setProvider(
-  new web3.providers.HttpProvider(
-    process.env.INFURA_URL
-  )
-)
+const web3 = new Web3();                // Create a new Web3 instance
 
-// Add private key
-const account = web3.eth.accounts.privateKeyToAccount(
-  process.env.ACCOUNT_PRIVATE_KEY
-)
-web3.eth.accounts.wallet.add(account)
+// Set the Ethereum provider (connection)
+web3.setProvider(new web3.providers.HttpProvider(process.env.INFURA_URL));
+// Use the INFURA_URL from the environment variables as the Ethereum provider
 
-const voter = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS)
+// Add the private key to the account for signing transactions
+const account = web3.eth.accounts.privateKeyToAccount(process.env.ACCOUNT_PRIVATE_KEY);
+// Convert the private key from the environment variables to an account object
+web3.eth.accounts.wallet.add(account);   // Add the account to the web3 wallet for signing transactions
 
-// Send transaction
+// Create a contract instance with the ABI and contract address
+const voter = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+// The contract instance allows interacting with the smart contract
+
+// Send a transaction to vote
 sendTransaction()
     .then(function() {
-        console.log('Done')
+        console.log('Done');
     })
     .catch(function(error) {
-        console.log('Failed to send a transaction: ', error)
-    })
+        console.log('Failed to send a transaction: ', error);
+    });
 
+// Async function to send a transaction to vote
 async function sendTransaction() {
-    console.log('Estimating gas')
+    console.log('Estimating gas');
+    // Estimate the gas required for the transaction
     const gasEstimate = await web3.eth.estimateGas({
         from: account.address,
         to: process.env.CONTRACT_ADDRESS,
         data: voter.methods['vote(uint256)'](0).encodeABI(),
-    })
-    console.log('Gas estimate: ', gasEstimate)
+    });
+    console.log('Gas estimate: ', gasEstimate);
 
-    console.log('Voting')
-    await voter
-        .methods['vote(uint256)'](0)
-        .send({
-            from: account.address,
-            gas: gasEstimate
-        })
+    console.log('Voting');
+    // Perform the actual voting by sending a transaction to the contract
+    await voter.methods['vote(uint256)'](0).send({
+        from: account.address,
+        gas: gasEstimate
+    });
 
-    console.log('Getting votes')
-    const votes = await voter.methods.getVotes().call({from: account.address})
+    console.log('Getting votes');
+    // Call the smart contract function to get the updated votes
+    const votes = await voter.methods.getVotes().call({ from: account.address });
 
-    console.log(`Votes: ${votes}`)
+    console.log(`Votes: ${votes}`);
 }
